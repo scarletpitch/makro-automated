82ae5d30eb0aa5efaddcb3bae163bd090bc6e89f
pisha m <pisham@rabbit.co.th>
2025-03-27 22:14:34 +0700

    add lighthouse ci

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['https://maknet-webapp.mango-qa.siammakro.cloud/en'],
+        numberOfRuns: 1
+      },
+      upload: {
+        target: 'temporary-public-storage'
+      },
+      assert: {
+        assertions: {
+          'categories:performance': ['warn', { minScore: 0.8 }]
+        }
+      }
+    }
+  };
+  
